b8df5dbbb60f149b14c9b97aad375ac01c73faf6
ZJU-DDRX <zjuddrx2023@outlook.com>
2026-05-21 16:00:26 +0800

    Prioritize team building in lab life

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,9 @@ if (toggle && nav) {
 const slides = Array.from(document.querySelectorAll(".life-slideshow img"));
 
 if (slides.length > 1) {
-  let activeSlide = slides.findIndex((slide) => slide.classList.contains("is-active"));
-  if (activeSlide < 0) {
-    activeSlide = 0;
-    slides[activeSlide].classList.add("is-active");
-  }
+  slides.forEach((slide) => slide.classList.remove("is-active"));
+  let activeSlide = Math.floor(Math.random() * slides.length);
+  slides[activeSlide].classList.add("is-active");
 
   window.setInterval(() => {
     slides[activeSlide].classList.remove("is-active");
